33cc93b276489b583735ae354fae50f04302c014
Dimi <114114664+DimiEmerick@users.noreply.github.com>
2025-05-26 16:40:59 -0300

    Added Game Manager

--- Assets/Scripts/Menu/MenuButtonManager.cs
+++ Assets/Scripts/Menu/MenuButtonManager.cs
@@ -7,17 +7,33 @@ public class MenuButtonManager : MonoBehaviour
 {
     public List<GameObject> buttons;
 
-    private void ShowButtons()
-    {
+    [Header("Animation")]
+    public float duration = .2f;
+    public float delay = .05f;
+    public Ease ease = Ease.OutBack;
 
+    private void Awake()
+    {
+        HideAllButtons();
+        ShowButtons();
     }
 
-    IEnumerator ShowButtonsWithDelay()
+    private void HideAllButtons()
     {
-        foreach(var b in buttons)
+        foreach (var b in buttons)
         {
+            b.transform.localScale = Vector3.zero;
+            b.SetActive(false);
+        }
+    }
+
+    private void ShowButtons()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            var b = buttons[i];
             b.SetActive(true);
-            b.transform.DOScale(1);
+            b.transform.DOScale(1, duration).SetDelay(i*delay).SetEase(ease);
         }
     }
 }
