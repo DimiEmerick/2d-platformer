222ecbd5918e5de4ecd12ba637a5d5bb643263d3
Dimi <114114664+DimiEmerick@users.noreply.github.com>
2025-05-29 14:36:21 -0300

    Added Run to Prototype

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,7 +7,10 @@ public class Player : MonoBehaviour
     public Rigidbody2D myRB;
     public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
+    public float speedRun;
     public float forceJump;
+
+    private float _currentSpeed;
 
     private void Update()
     {
@@ -17,15 +20,18 @@ public class Player : MonoBehaviour
 
     private void HandleMovement()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+            _currentSpeed = speedRun;
+        else
+            _currentSpeed = speed;
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            // myRB.MovePosition(myRB.position - velocity * Time.deltaTime);
-            myRB.velocity = new Vector2(-speed, myRB.velocity.y);
+            myRB.velocity = new Vector2(-_currentSpeed, myRB.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            // myRB.MovePosition(myRB.position + velocity * Time.deltaTime);
-            myRB.velocity = new Vector2(speed, myRB.velocity.y);
+            myRB.velocity = new Vector2(_currentSpeed, myRB.velocity.y);
         }
 
         if (myRB.velocity.x > 0)
