adb81228742f27efd3d85366145faedd3ac83816
Dimi <114114664+DimiEmerick@users.noreply.github.com>
2025-06-02 16:52:41 -0300

    Added health and damage base systems

---
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    public int damage = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.transform.name);
+        var health = collision.GetComponent<HealthBase>();
+        if (health != null)
+        {
+            health.Damage(damage);
+        }
+    }
+}
